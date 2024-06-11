9d08135c1d6c479eba11776b1c33cc68f337f3bc
김민석|Minseok Kim <alstjr7437@naver.com>
2024-06-12 00:30:07 +0900

    test

---
+++ iOS/banchango/banchangoApp.swift
@@ -0,0 +1,17 @@
+//
+//  banchangoApp.swift
+//  banchango
+//
+//  Created by KimMinSeok on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct banchangoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
